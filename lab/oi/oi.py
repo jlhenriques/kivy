36251b0e021469dce6cdcbd08836e42071ec9c27
Luciano Ramalho <luciano@ramalho.org>
2013-12-23 17:06:52 -0200

    botao que diz Oi

--- lab/oi/oi.py
+++ lab/oi/oi.py
@@ -6,9 +6,13 @@ https://www.youtube.com/watch?v=F7UKmK9eQLY
 """
 
 from kivy.app import App
+from kivy.uix.button import Button
 
 class OiApp(App):
-	pass
+	def build(self):
+		return Button(text='Oi!',
+					  background_color=(0, 0, 1, 1),
+					  font_size=150)
 
 if __name__=='__main__':
 	OiApp().run()
