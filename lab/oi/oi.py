f1f2b729bd43daec15677b9c0f76055fae49951c
Luciano Ramalho <luciano@ramalho.org>
2013-12-23 17:01:40 -0200

    tutorial youtube: janela preta OK

---
+++ lab/oi/oi.py
@@ -0,0 +1,14 @@
+"""
+Aprendendo Kivy
+
+Seguindo o tutorial de Alexander Taylor no YouTube:
+https://www.youtube.com/watch?v=F7UKmK9eQLY
+"""
+
+from kivy.app import App
+
+class OiApp(App):
+	pass
+
+if __name__=='__main__':
+	OiApp().run()
