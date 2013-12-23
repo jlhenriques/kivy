adcd58305e19f799542ebb18c0778f11cafdc155
Luciano Ramalho <luciano@ramalho.org>
2013-12-23 17:32:09 -0200

    exemplo com scatter: multi-toque para zoom e rotacionar so funciona uma vez, depois para...

--- lab/oi/oi.py
+++ lab/oi/oi.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 """
 Aprendendo Kivy
 
@@ -6,13 +8,19 @@ https://www.youtube.com/watch?v=F7UKmK9eQLY
 """
 
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.scatter import Scatter
+from kivy.uix.floatlayout import FloatLayout
 
 class OiApp(App):
-	def build(self):
-		return Button(text='Oi!',
-					  background_color=(0, 0, 1, 1),
-					  font_size=150)
+    def build(self):
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(text='Hello!',
+                  font_size=150)
+        f.add_widget(s)
+        s.add_widget(l)
+        return f
 
 if __name__=='__main__':
-	OiApp().run()
+    OiApp().run()
